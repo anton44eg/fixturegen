dde56578911efaf802a11fe7341becda4febb15d
Anton Simernia <anton.simernya@gmail.com>
2015-05-09 16:21:27 +0530

    add python version pypi classifiers [skip ci]

--- setup.py
+++ setup.py
@@ -40,6 +40,10 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Topic :: Software Development :: Testing',
         'Topic :: Database',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
     ],
     test_suite='test_fixturegen',
     setup_requires=[
